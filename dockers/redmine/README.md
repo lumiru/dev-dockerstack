3826599ef3d899e71bb44eaf824008710c0aba41
lumiru <lumiru@live.fr>
2015-04-12 01:28:58 +0200

    Add redmine 2.5 image

---
+++ dockers/redmine/README.md
@@ -0,0 +1,27 @@
+Redmine Docker Image by lumiru.
+	Based on "Java-default Image by NikoWoot".
+
+Included packages (in base image) : - nano
+									- zip, unzip
+									- openssh-server (sshd)
+									- ruby
+									- ruby-bundler
+
+Included packages : - rails (gem)
+					- make
+					- gcc
+
+Softwares is in default version of ubuntu 14.04 repositories.
+
+==========
+
+CREATED and MAINTENED BY
+Frédéric TURPIN <lumiru@turp.in>
+Nicolas GAUTIER <ngautier@enroot.fr>
+
+==========
+	Fork me, if you like my repository.
+==========
+
+=> I have a problem with your image :
+- You can send a mail for any questions.
---
+++ dockers/redmine/run.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# =========================================
+# Generate password (for production)
+# =========================================
+# $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+
+# =========================================
+# Activate SSHD, if variable is defined
+# =========================================
+if [ -n "$SSH_ROOT_PASSWORD" ]; then
+
+	# =========================================
+	# Change Root Password
+	# =========================================
+    echo "root:$SSH_ROOT_PASSWORD" | chpasswd
+
+    # =========================================
+	# Print in file and console
+	# =========================================
+    echo "SSHD ==> root:$SSH_ROOT_PASSWORD"
+    echo "SSHD ==> root:$SSH_ROOT_PASSWORD" > /sshd.txt
+
+    # ============================================
+	# SSHD - Configure access by Password or PAM
+	# ============================================
+	sed -ri 's/^PermitRootLogin\s+.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+	sed -ri 's/UsePAM yes/#UsePAM yes/g' /etc/ssh/sshd_config
+
+	# =========================================
+	# Launch SSHD
+	# =========================================
+	/usr/sbin/sshd -D
+fi
+
+# =========================================
+# Configure Redmine
+# =========================================
+if [ ! -f ~/firstrun ]; then
+	echo "production:
+  adapter: mysql2
+  database: $SQL_DB
+  port: $SQL_PORT
+  host: $SQL_HOST
+  username: $SQL_USER
+  password: $SQL_PASS
+  encoding: utf8" > /opt/redmine/config/database.yml
+
+	cd /opt/redmine
+
+	source /usr/local/rvm/scripts/rvm
+
+	DEBIAN_FRONTEND=noninteractive RAILS_ENV=production bundle exec rake db:migrate
+	DEBIAN_FRONTEND=noninteractive RAILS_ENV=production REDMINE_LANG=fr bundle exec rake redmine:load_default_data
+	DEBIAN_FRONTEND=noninteractive bundle exec rake generate_secret_token
+
+	touch /opt/redmine/tmp/restart.txt
+
+	# Save firstrun
+	echo "true" > ~/firstrun
+fi
+
+# =========================================
+# Launch NginX
+# =========================================
+echo "=> Launch NginX"
+/opt/nginx/sbin/nginx
