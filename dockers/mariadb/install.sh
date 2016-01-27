d8f5f9d6ab2baf568c5e944f6b14155d55d9a664
lumiru <lumiru@live.fr>
2016-01-27 02:13:55 +0100

    Utilisation des dépôts officiels pour mariadb

--- dockers/mariadb/install.sh
+++ dockers/mariadb/install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+PASS='root'
+
 # =========================================
 # Update apt-cache and install software (MariaDB-Server)
 # =========================================
-DEBIAN_FRONTEND=noninteractive echo "deb http://ftp.igh.cnrs.fr/pub/mariadb/repo/10.0/debian wheezy main" >> /etc/apt/sources.list && \
-							   apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db && \
+DEBIAN_FRONTEND=noninteractive debconf-set-selections <<< "mariadb-server-10.0 mysql-server/root_password password $PASS" && \
+							   debconf-set-selections <<< "mariadb-server-10.0 mysql-server/root_password_again password $PASS" && \
 							   apt-get update && \
 							   apt-get -yq --force-yes --no-install-recommends install mariadb-server && \
        						   apt-get clean && \
